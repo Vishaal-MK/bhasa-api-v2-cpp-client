15e9ee558db7baa751b58f955e5930111f64a289
Vishaal MK <vishaalmk@gmail.com>
2022-11-21 11:31:22 +0530

    Removed .cmake files

--- certify/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/vishaal/Desktop/VSCode/bhasa-api-v2-cpp-client/certify
-# Build directory: /home/vishaal/Desktop/VSCode/bhasa-api-v2-cpp-client/certify
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("tests")
